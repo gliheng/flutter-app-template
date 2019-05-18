818bb17cd3567a72c433c10a12c83f79e923188c
juju <gliheng@gmail.com>
2019-05-18 17:26:44 +0800

    Update for flutter-engine@0.3.0

--- README.md
+++ README.md
@@ -7,60 +7,58 @@ Example app built using flutter-rs.
 ![screenshot](https://raw.githubusercontent.com/gliheng/flutter-rs/master/www/images/screenshot_mac.png)
 
 
-- Runs on mac and linux, so far.
-- Build distribution only works on mac.
-- Windows will be supported.
+- Support Hot reload
+- MethodChannel, EventChannel
+- Async runtime using tokio
+- System dialogs
+- Clipboard support
+- Cross platform support, Runs on mac, windows, linux
+- Support distribution format: (windows NSIS, mac app, mac dmg, linux snap)
 
-# Requirement
+# Get Started
+
+## Install requirements
 
 - [Rust](https://www.rust-lang.org/tools/install)
-- cmake:
-    - Install on Mac with: `brew install cmake`
-    - Install on linux with `apt install cmake`
+
 - libglfw:
     - Install on Mac with: `brew install glfw`
     - Install on linux with `apt install libglfw3`
+- cmake:
+    - Install on Mac with: `brew install cmake`
+    - Install on linux with `apt install cmake`
+    
+- [flutter sdk](https://flutter.io)
 
-# Get Started
-## Clone
+## Config flutter engine version
+flutter-rs need to know your flutter engine version.
+You can set this using any of the following methods.
+- If you have flutter cli in your PATH, you're set.
+- Set FLUTTER_ROOT environment variable to your flutter sdk path
+- Set FLUTTER_ENGINE_VERSION environment variable to your engine version
+
+## Clone this repo
 
     git clone git@github.com:gliheng/flutter-app-template.git flutter-app
     cd flutter-app
-    ./scripts/init.py
-
-## Config
-- Set flutter engine version in *Cargo.toml*
-
-```
-[package.metadata.flutter]
-version = "5af435098d340237c5e3a69bce6aaffd4e3bfe84"
-```
-
-    This commit version id can be found in bin/internal/engine.version file in flutter sdk folder.
+    python ./scipts/init.py
 
 ## Develop
+- To develop with cli hot-reloading, simple run:
 
-- To develop with hot-reloading, use:
-    `./scripts/run.py`
+    `python ./scripts/run.py`
 
-    To show rust logs, add these environment variables: `RUST_LOG=flutter_engine=trace,flutter_app=trace ./scripts/run.py`
+- To debug using VS Code dart tools:
+
+    Start process using `cargo run`
+
+    Then attach to debugger using
+    `flutter attach --debug-uri=DEBUG_URI`
 
 ## Distribute
-
 - To build distribution, use:
-    `./scripts/build.py mac`
-    `./scripts/build.py dmg`
+    `python ./scripts/build.py mac|dmg|nsis|snap`
 
-**Note:**
-Build scripts are written in python3. Install python depenendencies using `pip3 install -r scripts/requirements.txt`
-
----
-
-### For users in China
-Please ensure you have access to *storage.googleapis.com*. It is required to download lib_flutter. 
-
-Set appropriate http proxy in the terminal by using:
-```shell
-export http_proxy=...
-export https_proxy=...
-```
+    **Note:**
+    Build scripts are written in python3. Install python depenendencies using `pip3 install -r scripts/requirements.txt`
+    Build on Windows require [NSIS3](https://sourceforge.net/projects/nsis/files/NSIS%203/)
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,10 +1,10 @@
-use std::{
-    env,
-    path::PathBuf,
-};
-use log::{info};
-use env_logger;
-use flutter_engine::{FlutterEngineArgs, FlutterEngine, WindowMode};
+// This build a windows app without console on windows in release mode
+#![cfg_attr(all(windows, not(debug_assertions)), windows_subsystem = "windows")]
+
+use std::{env, path::PathBuf};
+
+use fern::colors::{Color, ColoredLevelConfig};
+use log::info;
 
 #[cfg(target_os = "macos")]
 use core_foundation::bundle;
@@ -12,50 +12,80 @@ use core_foundation::bundle;
 #[cfg(target_os = "macos")]
 fn get_res_dir() -> PathBuf {
     let bd = bundle::CFBundle::main_bundle();
-    let exe = bd.executable_url().expect("Cannot get executable dir").to_path().expect("to_path error");
+    let exe = bd
+        .executable_url()
+        .expect("Cannot get executable dir")
+        .to_path()
+        .expect("to_path error");
     exe.parent().unwrap().parent().unwrap().join("Resources")
 }
 
 #[cfg(not(target_os = "macos"))]
 fn get_res_dir() -> PathBuf {
-    env::current_exe().expect("Cannot get application dir")
-        .parent().expect("Cannot get application dir")
+    env::current_exe()
+        .expect("Cannot get application dir")
+        .parent()
+        .expect("Cannot get application dir")
         .to_path_buf()
 }
 
 fn main() {
-    env_logger::init();
-    flutter_engine::init();
+    let colors = ColoredLevelConfig::new()
+        .error(Color::Red)
+        .warn(Color::Yellow)
+        .info(Color::Green)
+        .debug(Color::White)
+        .trace(Color::BrightBlack);
+
+    fern::Dispatch::new()
+        .level(log::LevelFilter::Debug)
+        .format(move |out, message, record| {
+            out.finish(format_args!(
+                "{}[{}][{}][{}] {}\x1B[0m",
+                format_args!("\x1B[{}m", colors.get_color(&record.level()).to_fg_str()),
+                chrono::Local::now().format("%H:%M:%S"),
+                record.level(),
+                record.target(),
+                message
+            ))
+        })
+        .chain(std::io::stdout())
+        .apply()
+        .unwrap();
 
     let (assets_path, icu_data_path) = match env::var("CARGO_MANIFEST_DIR") {
         Ok(proj_dir) => {
             info!("Running inside cargo project");
             let proj_dir = PathBuf::from(&proj_dir);
             (
-                proj_dir.parent().unwrap().join("build").join("flutter_assets"),
+                proj_dir
+                    .parent()
+                    .unwrap()
+                    .join("build")
+                    .join("flutter_assets"),
                 proj_dir.join("assets/icudtl.dat"),
             )
-        },
+        }
         Err(_) => {
             let res = get_res_dir();
-            (
-                res.join("flutter_assets"),
-                res.join("icudtl.dat"),
-            )
-        },
+            (res.join("flutter_assets"), res.join("icudtl.dat"))
+        }
     };
 
-    let args = FlutterEngineArgs{
-        assets_path: assets_path.to_string_lossy().into_owned(),
-        icu_data_path: icu_data_path.to_string_lossy().into_owned(),
-        title: String::from("Flutter Demo"),
-        width: 800,
-        height: 600,
-        window_mode: WindowMode::Windowed,
-        ..Default::default()
-    };
-
-    let engine = FlutterEngine::new(args);
-    engine.run();
-    engine.shutdown();
+    let mut engine = flutter_engine::init().unwrap();
+    engine
+        .create_window(
+            &flutter_engine::WindowArgs {
+                height: 768,
+                width: 1024,
+                title: "Flutter Demo",
+                mode: flutter_engine::WindowMode::Windowed,
+                bg_color: (255, 255, 255),
+            },
+            assets_path.to_string_lossy().to_string(),
+            icu_data_path.to_string_lossy().to_string(),
+            vec![],
+        )
+        .unwrap();
+    engine.run_window_loop(None, None);
 }
